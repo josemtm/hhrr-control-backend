3a1e908f4a4ff539275cde5fca66dab948c83ae0
joseT <jmtm_2002@hotmail.com>
2019-08-16 04:55:52 -0430

    Projecto iniciado

---
+++ src/main/java/hhrr/ControlBackendApplication.java
@@ -0,0 +1,13 @@
+package hhrr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hhrr/ControlBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package hhrr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ControlBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
